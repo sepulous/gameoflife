ca435337a9e0a1e7f5a2f9248af9aef68e1113d9
sepulous <minerabacuss@gmail.com>
2020-11-28 17:04:39 -0600

    Made minor improvements to code

--- main.py
+++ main.py
@@ -12,31 +12,29 @@ WINDOW_TITLE = f'Game of Life v{VERSION}'
 
 STATE_RUNNING = 0
 STATE_PAUSED = 1
-STATE_STOPPED = 2
+STATE_RESET = 2
 
 
 def main():
     pygame.init()
     pygame.display.set_caption(WINDOW_TITLE)
     
-    os.environ['SDL_VIDEO_CENTERED'] = '1' # Center window
+    os.environ['SDL_VIDEO_CENTERED'] = '1' # Center the window
 
     window_height = int(pygame.display.Info().current_h * 0.8)
     window_width = window_height
-
     window = pygame.display.set_mode((window_width, window_height))
 
     font = pygame.font.SysFont('Comic Sans MS', 18)
     
-    matrix_order = 30
-    line_width = 2
+    matrix_order = 40
+    line_width = 1
 
     cell_size = (window_width / matrix_order) - line_width
     cell_matrix = CellMatrix(matrix_order)
 
     rectangles = []
-    x_pos = 0
-    y_pos = 0
+    x_pos, y_pos = 0, 0
     for index in range(matrix_order**2):
         rectangles.append(pygame.Rect(x_pos, y_pos, cell_size, cell_size))
         if (index + 1) % matrix_order == 0:
@@ -46,48 +44,46 @@ def main():
             x_pos += cell_size + line_width
 
 
-    game_state = STATE_STOPPED
+    game_state = STATE_RESET
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == 32: # Space
+                if event.key == pygame.K_SPACE:
                     if game_state == STATE_RUNNING:
                         game_state = STATE_PAUSED
                     else:
                         game_state = STATE_RUNNING
-                elif event.key == 27: # Escape
-                    game_state = STATE_STOPPED
+                elif event.key == pygame.K_ESCAPE:
+                    game_state = STATE_RESET
                     cell_matrix.reset()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # Left click
                 if game_state != STATE_RUNNING:
                     mouse_x, mouse_y = event.pos[0], event.pos[1]
                     i, j = 0, 0
                     for (count, rect) in enumerate(rectangles, start=1):
-                        if (rect.x <= mouse_x <= rect.x + cell_size) and (rect.y <= mouse_y <= rect.y + cell_size):
+                        between_x_bounds = rect.x <= mouse_x <= rect.x + cell_size + line_width
+                        between_y_bounds = rect.y <= mouse_y <= rect.y + cell_size + line_width
+                        if between_x_bounds and between_y_bounds:
                             break
-
-                        if count % matrix_order == 0:
+                        elif count % matrix_order == 0:
                             i += 1
                             j = 0
                         else:
                             j += 1
 
-                    if (0 <= i < matrix_order) and (0 <= j < matrix_order):
-                        cell_matrix.toggle_cell(i, j)
+                    cell_matrix.toggle_cell(i, j)
 
 
         if game_state == STATE_RUNNING:
             time.sleep(0.2)
             cell_matrix.update()
 
-        window.fill(0);
+        window.fill(0)
         for (rect, cell) in zip(rectangles, cell_matrix.get_cells()):
-            if cell.alive:
-                pygame.draw.rect(window, 0, rect) 
-            else:
-                pygame.draw.rect(window, 0xffffff, rect)
+            color = 0x0 if cell.alive else 0xffffff
+            pygame.draw.rect(window, color, rect)
 
 
         pygame.draw.rect(window, 0xffffff, pygame.Rect(0, 0, int(window_width * 0.35), 50))
